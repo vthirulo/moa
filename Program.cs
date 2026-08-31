af4f47fe9807512b09278152fab641cc6293aa2d
vthirulo <224445468+vthirulo@users.noreply.github.com>
2026-08-31 22:21:40 +0530

    [Scanner] User Input

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+public class MoaProgram
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: dotnet run [file]");
+            Console.WriteLine("Usage: dotnet run");
+        }
+        else if (args.Length == 1)
+        {
+            Moa.Scanner.ReadFile(args[0]);
+        }
+        else{
+            Moa.Scanner.Prompt();
+        }
+    }
+}
