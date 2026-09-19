b32fa3f7fca4b891b8b72aa4c84cb0e827eddcbd
vthirulo <224445468+vthirulo@users.noreply.github.com>
2026-09-19 21:11:20 +0530

    [Generator] added visitor pattern

--- generate_ast.py
+++ generate_ast.py
@@ -4,27 +4,44 @@ import sys
 
 output_dir:str
 
-def DefineAST(ast_file, output_dir:str, base_name:str, types:list[str]):
+def DefineAST(ast_file, output_dir:str, base_class_name:str, types:list[str]):
 
-    print("\nabstract class " + base_name, file=ast_file)
+    print("\nabstract class " + base_class_name, file=ast_file)
     print("{", file=ast_file)
-    print(file=ast_file)
+    DefineVisitor(ast_file, base_class_name, types)
+    print("\tpublic abstract R Accept<R>(IVisitor<R> visitor);", file=ast_file)
     print("}", file=ast_file)
 
     for type in types:
         in_class_name = type.split(':')[0].replace(" ", "")
         fields = type.split(":")[1].strip()
-        DefineTypes(ast_file, base_name, in_class_name, fields)
+        print("\n", file=ast_file)
+        DefineTypes(ast_file, base_class_name, in_class_name, fields)
 
-def DefineTypes(ast_file, base_name:str, class_name:str, fields:str):
-    print("class " + class_name + " : " + base_name, file=ast_file)
+def DefineTypes(ast_file, base_class_name:str, class_name:str, fields:str):
+    print("class " + class_name + " : " + base_class_name, file=ast_file)
     print("{", file=ast_file)
 
     FieldList = fields.split(', ')
     for a_field in FieldList:
         print("\t" + a_field + ";", file=ast_file)
 
+    print("\n\tpublic override R Accept<R>(IVisitor<R> visitor)", file=ast_file)
+    print("\t{", file=ast_file)
+    print("\t\treturn visitor.visit" + class_name + base_class_name + "(this);", file=ast_file)
+    print("\t}", file=ast_file)
+
     print("}", file=ast_file)
+
+def DefineVisitor(ast_file, base_class_name:str, types:list[str]):
+    print("\tpublic interface IVisitor<R>", file=ast_file)
+    print("\t{", file=ast_file)
+
+    for type in types:
+        typeName = type.split(':')[0].replace(" ", "")
+        print("\t\tR visit" + typeName + base_class_name + "(" + typeName + " expr);", file=ast_file)
+
+    print("\t}", file=ast_file)
 
 if len(sys.argv) == 1:
     print("Usage: generate_ast.py [output directory]")
@@ -36,6 +53,8 @@ else:
     with open(path, mode="w", encoding="utf-8") as ast_file:
         print("\nnamespace Moa;", file=ast_file)
         DefineAST(ast_file, output_dir, "Expression", [
-            "Binary     : Expression Left, Token Middle, Expression Right",
-            "Grouping   : Expression Expr"
+            "Unary      : Token? _operator, Expression? _right",
+            "Binary     : Expression? _left, Token? _operator, Expression? _right",
+            "Grouping   : Expression? _expr",
+            "Literal    : Object? _value",
         ])
